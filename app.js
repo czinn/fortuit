9ec132491af74ec4b44477a56881f69dbf2fdc23
Charles Zinn <charles.zinn@gmail.com>
2015-01-16 20:03:23 -0500

    Added dependencies.

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/lib', express.static(path.join(__dirname, 'bower_components')));
 
 app.use('/', routes);
 app.use('/users', users);
