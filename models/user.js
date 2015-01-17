8fc1fb02e202dfa53da238eb998235d7b26a5183
Charles Zinn <charles.zinn@gmail.com>
2015-01-16 21:07:29 -0500

    Automatically hashes passwords when creating users or changing passwords.

--- models/user.js
+++ models/user.js
@@ -10,6 +10,19 @@ UserSchema.methods.validPassword = function(password) {
   return bcrypt.compareSync(password, this.password);
 };
 
+UserSchema.pre('save', function(next) {
+  var user = this;
+
+  if(!user.isModified('password')) return next();
+
+  bcrypt.hash(user.password, null, null, function(err, hash) {
+    if(err) return next(err);
+
+    user.password = hash;
+    next();
+  });
+});
+
 var User = mongoose.model('User', UserSchema);
 
 module.exports = User;
